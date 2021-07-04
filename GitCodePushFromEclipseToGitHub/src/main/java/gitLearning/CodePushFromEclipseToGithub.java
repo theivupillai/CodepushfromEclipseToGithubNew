30c17df0da8da452ec0cf4ed1f9f59ea5db39541
Theivu <Theivu@LAPTOP-A0ABFCSE>
2021-07-04 20:39:37 +0530

    Initial Commit

---
+++ GitCodePushFromEclipseToGitHub/src/main/java/gitLearning/CodePushFromEclipseToGithub.java
@@ -0,0 +1,15 @@
+package gitLearning;
+
+// Remote Repository name = CodepushfromEclipseToGithubNew
+// URL: https://github.com/theivupillai/CodepushfromEclipseToGithubNew.git
+
+public class CodePushFromEclipseToGithub {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to GitHub Learning!!! ");
+		
+		
+	}
+
+}
